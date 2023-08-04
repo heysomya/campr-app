fb426f8662046749c99125bd03c55d56f23f0297
heysomya <who.somya321@gmail.com>
2023-08-04 14:36:27 +0530

    added basic styles and layouts

--- seeds/index.js
+++ seeds/index.js
@@ -17,11 +17,16 @@ const sample = (array) => {
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
-    randomNum = Math.floor(Math.random() * 1000) + 1;
+  for (let i = 0; i < 30; i++) {
+    const random1000 = Math.floor(Math.random() * 1000) + 1;
+    const randomNum = Math.floor(Math.random() * 30) + 10;
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
-      location: `${cities[randomNum].city}, ${cities[randomNum].state}`,
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: `https://source.unsplash.com/random/400x300/?camping,${i}`,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni recusandae mollitia rerum distinctio expedita, enim dolorem ex perferendis facilis cumque exercitationem suscipit, consequatur quibusdam temporibus, aspernatur incidunt quod veniam eius!",
+      price: randomNum,
     });
     await camp.save();
   }
