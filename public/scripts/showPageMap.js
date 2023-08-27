ae5119a280b2b970bffc55722c7e7aff445ee0ca
heysomya <who.somya321@gmail.com>
2023-08-27 13:31:15 +0530

    add homepage and other styles

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -6,11 +6,12 @@ const map = new mapboxgl.Map({
   zoom: 10, // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
   .setPopup(
-    new mapboxgl.Popup({ offset: 25 })
-    .setHTML(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
       `<h6>${campground.title}</h6><p>${campground.location}</p>`
     )
   )
